9f9cd029bfef442487ac55539378a3be95e063f5
Ivan Gavrilov <gisne@ya.ru>
2017-09-11 23:22:38 +0300

    add echo in install_ruby script

--- install_ruby.sh
+++ install_ruby.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+echo "===> Installing RVM >>"
 sudo gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable
-sudo source ~/.rvm/scripts/rvm
-sudo rvm requirements && sudo rvm install 2.4.1 && sudo rvm use 2.4.1 --default
+. ~/.rvm/scripts/rvm
+
+echo '===> Installing requirements'
+rvm requirements 
+
+echo '===> Installing Ruby 2.4.1'
+sudo rvm install 2.4.1
+
+echo '===> Setting default to 2.4.1'
+sudo rvm use 2.4.1 --default
+
 gem install bundler -V --no-ri --no-rdoc
-ruby -v
