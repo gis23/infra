37f5a0723ac0d30ab9049160fd6cfbf3cf793bd0
Ivan Gavrilov <gisne@ya.ru>
2017-09-11 23:46:46 +0300

    remove sudo from install_ruby

--- install_ruby.sh
+++ install_ruby.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "===> Installing RVM >>"
-sudo gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable
 source ~/.rvm/scripts/rvm
 
@@ -9,9 +9,9 @@ echo '===> Installing requirements'
 rvm requirements
 
 echo '===> Installing Ruby 2.4.1'
-sudo rvm install 2.4.1
+rvm install 2.4.1
 
 echo '===> Setting default to 2.4.1'
-sudo rvm use 2.4.1 --default
+rvm use 2.4.1 --default
 
 gem install bundler -V --no-ri --no-rdoc
