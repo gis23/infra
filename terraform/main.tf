7567fafd1a0156d42399bc436caa89833465c7af
Ivan Gavrilov <gisne@ya.ru>
2017-09-21 00:04:40 +0300

    add terraform to infra. IAC

---
+++ terraform/main.tf
@@ -0,0 +1,55 @@
+provider "google" {
+  project = "infra-180517"
+  region = "europe-west1"
+}
+
+resource "google_compute_instance" "app" {
+  name = "reddit-app"
+  machine_type = "g1-small"
+  zone = "europe-west1-b"
+
+  boot_disk {
+    initialize_params {
+      image = "reddit-base-1505931307"
+    }
+  }
+
+  metadata {
+  sshKeys = "appuser:${file("~/.ssh/appuser.pub")}"
+  }
+
+  tags = ["reddit-app"]
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+
+  connection {
+    type = "ssh"
+    user = "appuser"
+  }
+
+  provisioner "file" {
+    source = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
+
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+    }
+  }
+
+resource "google_compute_firewall" "firewall_puma" {
+  name = "allow-puma-default"
+  # Название сети, в которой действует правило
+  network = "default"
+  # Какой доступ разрешить
+  allow {
+  protocol = "tcp"
+  ports = ["9292"]
+  }
+  # Каким адресам разрешаем доступ
+  source_ranges = ["0.0.0.0/0"]
+  # Правило применимо для инстансов с тегом ...
+  target_tags = ["reddit-app"]
+}
